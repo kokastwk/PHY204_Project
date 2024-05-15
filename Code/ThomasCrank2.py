fafd7b206ac04a70800e03a83bc0494b17821e92
kokastwk <58683071+kokastwk@users.noreply.github.com>
2024-05-15 02:33:30 +0200

    Added error/evolution diagnostics when E = 0

--- Code/ThomasCrank2.py
+++ Code/ThomasCrank2.py
@@ -5,9 +5,8 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
 
 class CrankNicolsonSolver:
-    def __init__(self, D, V, f, L, T, J, N, u0):
+    def __init__(self, D, f, L, T, J, N, u0):
         self.D = D  # Diffusion coefficient
-        self.V = V  # Velocity
         self.f = f  # Source term function
         self.L = L  # Spatial domain length
         self.T = T  # Total time
@@ -15,15 +14,7 @@ class CrankNicolsonSolver:
         self.N = N  # Number of time steps
         self.dx = L / J  # Spatial step size
         self.dt = T / N  # Time step size
-        self.sigma = D * self.dt / (2 * self.dx ** 2)
-
-        # self.nu = V * self.dt / (4 * self.dx)
-
-        A = D / (4 * self.dx ** 2)
-
-        arr = -1 / (np.arange(J - 1) + 1) * A
-
-        self.nu = V * self.dt / (4 * self.dx) + arr * self.dt
+        self.sigma = D * self.dt / (2 * (self.dx ** 2))
 
         self.x = np.linspace(0, L, J, endpoint=False)  # Spatial grid
         self.t = np.linspace(0, T, N, endpoint=False)  # Temporal grid
@@ -33,29 +24,17 @@ class CrankNicolsonSolver:
 
     def create_matrix_A(self):
         main_diag = (1 + 2 * self.sigma) * np.ones(self.J,dtype=np.complex128)
-        off_diag = -(self.sigma + self.nu)* np.ones(self.J-1,dtype=np.complex128)
-        upper_diag = -(self.sigma - self.nu)* np.ones(self.J-1,dtype=np.complex128)
+        off_diag = -(self.sigma)* np.ones(self.J-1,dtype=np.complex128)
+        upper_diag = -(self.sigma)* np.ones(self.J-1,dtype=np.complex128)
 
         A = np.diag(main_diag) + np.diag(off_diag,-1) + np.diag(upper_diag,1)
-        #print(self.x)
-        #print(len(A[0]))
-        #for dirichlet condition with zero at ends
-        #for i in range(J):
-            #A[0, i] = 0
-            #A[i,0] = 0
-            #A[J-1,i] = 0
-            #A[i,J-1] = 0
-
-        #A[0, 1] = - 2 * self.sigma
-
-        #A[self.J - 2, self.J - 3] = - 2 * self.sigma
 
         return A
 
     def create_matrix_B(self):
         main_diag = (1 - 2 * self.sigma) * np.ones(self.J,dtype=np.complex128)
-        off_diag = (self.sigma + self.nu)* np.ones(self.J-1,dtype=np.complex128)
-        upper_diag = (self.sigma - self.nu)* np.ones(self.J-1,dtype=np.complex128)
+        off_diag = (self.sigma)* np.ones(self.J-1,dtype=np.complex128)
+        upper_diag = (self.sigma)* np.ones(self.J-1,dtype=np.complex128)
 
         B = np.diag(main_diag) + np.diag(off_diag,-1) + np.diag(upper_diag,1)
         # for dirichlet condition with zero at ends
@@ -103,22 +82,21 @@ class CrankNicolsonSolver:
 
         self.U = U
 
-
+#L = 1 is about a thousand bohr radii and T = 1 is about a thousand evolutions of the H ground state
 hbarbym = 1
 oneoverhbar = 1
 coloumbbyplanck = 1
 # Domain and problem parameters
-L = 1  # Length of the domain
+L = 1 # Length of the domain
 T = 1e-3  # Total time
 J = 1000  # Number of spatial steps
 N = 10000 # Number of time steps
 # for J = 1000, stability until 0.5. 0.5 Unstable at J= 3000. J variation stability seems to be increasing with length
 D = (0.5j)*hbarbym
-V = 0.0
 
 # Source term function (set to 0 for simplicity)
 def f(u,x):
-    E = 2e-2
+    E = 0#5e-2
     if x == 0 :
         return 0
     else:
@@ -128,9 +106,9 @@ def f(u,x):
 # Initialize and solve
 def u_0(x):
     a = 1.11211e-2# bohr radius
-    return x*(a**(-3/2))*np.exp(-x/a)/np.sqrt(np.pi) #U1,0,0 state
+    return (x*np.exp(-x/a))/np.sqrt((a**3)*np.pi) #U1,0,0 state
 
-solver = CrankNicolsonSolver(D, V, f, L, T, J, N, u_0)
+solver = CrankNicolsonSolver(D, f, L, T, J, N, u_0)
 solver.simulate()
 final_solution = solver.U
 
@@ -144,10 +122,10 @@ final_solution = solver.U
 
 
 def probd(u):
-    return [((np.abs(u[k])/k)**2)*(J/L) for k in range(1,len(u))]
+    return [((np.abs(u[k]))**2)*((L/J)) for k in range(len(u))] # |psi|^2 r^2 dx = u[k]^2 (L/J)
 
 def rexpec(u):
-    return np.sum([((np.abs(u[k]))**2)/k for k in range(1,len(u))])
+    return np.sum([((np.abs(u[k]))**2)*k*((L/J)**2) for k in range(len(u))])  # r |psi|^2 r^2 dx = (k*L/J) u[k]^2  (L/J)
 
 def psinorm(u):
     return np.sum(probd(u))
@@ -157,10 +135,39 @@ def psinorm(u):
 
 
 
+'''delta curves'''
+
+#plot psinorm vs t
+plt.figure(figsize=(10, 5))
+plt.plot([t*T/N for t in range(len(final_solution))], [psinorm(final_solution[t] - final_solution[0]) for t in range(len(final_solution))], linestyle='--')
+plt.xlabel('t')
+plt.ylabel('Psinorm(t)')
+plt.title('Delta Psinorm(t)')
+plt.show()
+print("Done")
+
+#plot probd vs x
+plt.figure(figsize=(10, 5))
+plt.plot([x*L/J for x in range(len(final_solution[-1]))], probd(final_solution[-1] - final_solution[0]), linestyle='--')
+plt.xlabel('x')
+plt.ylabel('ProbDens')
+plt.title(' Delta Psi Density at Final Time')
+plt.show()
+print("Done")
+
+#plot expecr vs t
+plt.figure(figsize=(10, 5))
+plt.plot([t*T/N for t in range(len(final_solution))], [rexpec(final_solution[t] - final_solution[0]) for t in range(len(final_solution))], linestyle='--')
+plt.xlabel('t')
+plt.ylabel('Delta psi Rexpec')
+plt.title('Rexpec(t)')
+plt.show()
+print("Done")
 
 
 
 
+'''Data Curves'''
 #plot psinorm vs t
 plt.figure(figsize=(10, 5))
 plt.plot([t*T/N for t in range(len(final_solution))], [psinorm(final_solution[t]) for t in range(len(final_solution))], linestyle='--')
@@ -170,17 +177,25 @@ plt.title('Psinorm(t)')
 plt.show()
 print("Done")
 
+#plot probd vs x
+plt.figure(figsize=(10, 5))
+plt.plot([x*L/J for x in range(len(final_solution[0]))], probd(final_solution[0]), linestyle='--')
+plt.xlabel('x')
+plt.ylabel('ProbDens')
+plt.title('Probability Density at Initial Time')
+plt.show()
+print("Done")
 
 #plot probd vs x
 plt.figure(figsize=(10, 5))
-plt.plot([x*L/J for x in range(1,len(final_solution[len(final_solution)-1]))], probd(final_solution[len(final_solution)-1]), linestyle='--')
+plt.plot([x*L/J for x in range(len(final_solution[len(final_solution)-1]))], probd(final_solution[len(final_solution)-1]), linestyle='--')
 plt.xlabel('x')
 plt.ylabel('ProbDens')
 plt.title('Probability Density at Final Time')
 plt.show()
 print("Done")
 
-#plot rexpec vs t
+#plot expecr vs t
 plt.figure(figsize=(10, 5))
 plt.plot([t*T/N for t in range(len(final_solution))], [rexpec(final_solution[t]) for t in range(len(final_solution))], linestyle='--')
 plt.xlabel('t')
@@ -189,6 +204,23 @@ plt.title('Rexpec(t)')
 plt.show()
 print("Done")
 
+
+
+'''Function Curves'''
+k = 0
+
+# print(len(solver.U))
+# Plot the numerical and analytical solutions for comparison
+plt.figure(figsize=(10, 5))
+plt.plot(solver.x[1:], [(final_solution[k][i].real)/(i*(L/J)) for i in range(1,len(final_solution[k]))], linestyle='--')
+plt.plot(solver.x[1:], [(final_solution[k][i].imag)/(i*(L/J)) for i in range(1,len(final_solution[k]))], linestyle='--')
+plt.plot(solver.x[1:],[np.linalg.norm(final_solution[k][i])/(i*(L/J)) for i in range(1,len(final_solution[k]))], linestyle='--')
+#plt.plot(solver.x, u_analytical, label='Analytical Solution', linestyle='-.')
+plt.xlabel('x')
+plt.ylabel('psi(x,T)')
+plt.title('Wavefunction versus space at T = '+str(k*T/N))
+plt.show()
+print("Done")
 
 
 
@@ -203,7 +235,7 @@ plt.plot(solver.x[1:],[np.linalg.norm(final_solution[k][i])/(i*(L/J)) for i in r
 #plt.plot(solver.x, u_analytical, label='Analytical Solution', linestyle='-.')
 plt.xlabel('x')
 plt.ylabel('psi(x,T)')
-plt.title('Wavefunction versus space at T = '+str(T))
+plt.title('Wavefunction versus space at T = '+str(k*T/N))
 plt.show()
 print("Done")
 
@@ -222,3 +254,4 @@ plt.ylabel('psi(x,T)')
 plt.title('Wavefunction versus space at T = '+str(T))
 plt.show()
 print("Done")
+
